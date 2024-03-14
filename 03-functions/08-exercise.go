9170bad8a7bf6c1ab21c744a913ce766cebc6ca1
Magesh Kuppan <tkmagesh77@gmail.com>
2024-03-14 09:15:41 +0530

    excercise solution

--- 03-functions/08-exercise.go
+++ 03-functions/08-exercise.go
@@ -8,36 +8,73 @@ package main
 import "fmt"
 
 func main() {
-	var userChoice, n1, n2, result int
+
+	var (
+		userChoice, n1, n2, result int
+		op                         func(int, int) int
+	)
 
 	for {
-
-		fmt.Println("1. Add")
-		fmt.Println("2. Subtract")
-		fmt.Println("3. Multiply")
-		fmt.Println("4. Divide")
-		fmt.Println("5. Exit")
-		fmt.Println("Enter your choice :")
-		fmt.Scanln(&userChoice)
+		userChoice = getUserChoice()
 		if userChoice == 5 {
 			break
 		}
-		if userChoice < 1 || userChoice > 5 {
+		op = getOperation(userChoice)
+		if op == nil {
 			fmt.Println("Invalid choice")
 			continue
 		}
-		fmt.Println("Enter the operands :")
-		fmt.Scanln(&n1, &n2)
-		switch userChoice {
-		case 1:
-			result = n1 + n2
-		case 2:
-			result = n1 - n2
-		case 3:
-			result = n1 * n2
-		case 4:
-			result = n1 / n2
-		}
+		n1, n2 = getOperands()
+		result = op(n1, n2)
 		fmt.Println("Result :", result)
 	}
+}
+
+func getOperation(userChoice int) func(int, int) int {
+	switch userChoice {
+	case 1:
+		return add
+	case 2:
+		return subtract
+	case 3:
+		return multiply
+	case 4:
+		return divide
+	default:
+		return nil
+	}
+}
+
+func add(n1, n2 int) int {
+	return n1 + n2
+}
+
+func subtract(n1, n2 int) int {
+	return n1 - n2
+}
+
+func multiply(n1, n2 int) int {
+	return n1 * n2
+}
+
+func divide(n1, n2 int) int {
+	return n1 / n2
+}
+
+func getOperands() (n1, n2 int) {
+	fmt.Println("Enter the operands :")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func getUserChoice() int {
+	var userChoice int
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter your choice :")
+	fmt.Scanln(&userChoice)
+	return userChoice
 }
