0c25d34a2a5a8b94432a39f4b5c83d81da1b25ad
Magesh Kuppan <tkmagesh77@gmail.com>
2024-03-14 10:04:36 +0530

    nested errors

---
+++ 04-errors/03-nested-errors.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrF1 = errors.New("f1 error")
+var ErrF2 = errors.New("f2 error")
+
+func main() {
+	err := f1()
+	if err == nil {
+		fmt.Println("Operation successful")
+		return
+	}
+	fmt.Printf("type : %T\n value : %v\n", err, err)
+	if errors.Is(err, ErrF1) {
+		fmt.Println("f1 error occurred")
+	}
+	if errors.Is(err, ErrF2) {
+		fmt.Println("f2 error occurred")
+	}
+}
+
+func f1() error {
+	f2Err := f2()
+	/*
+		if f2Err != nil {
+			return ErrF1
+		}
+		return nil
+	*/
+	return fmt.Errorf("%w, %w", ErrF1, f2Err)
+}
+
+func f2() error {
+	return ErrF2
+}
